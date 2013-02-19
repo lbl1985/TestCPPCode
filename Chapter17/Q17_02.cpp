3737ea4bf4ccf41397a17d79dce660c91d039b16
Binlong Li <herbert19lee@gmail.com>
2013-02-18 20:19:12 -0500

    Q17_02 problem implement algorithm for N x N table

--- Chapter17/Q17_02.cpp
+++ Chapter17/Q17_02.cpp
@@ -6,4 +6,77 @@
 //  Copyright (c) 2013 Binlong Li. All rights reserved.
 //
 
-#include "Q17_02.h"
+#include <iostream>
+#include "../Utility/Random2DArray.cpp"
+
+
+int checkWin(int** const table, int size){
+    // check rows
+    for (int c = 1; c <= 2; c++) {
+        
+        
+        for (int i = 0; i < size; i++) {
+            bool rowCheck = true;
+            for (int j = 0; j < size; j++) {
+                if (table[i][j] != c) {
+                    rowCheck = false;
+                }
+            }
+            if (rowCheck) {
+                return c;
+            }
+        }
+        
+        for (int j = 0; j < size; j++) {
+            bool colCheck = true;
+            for (int i = 0; i < size; i++) {
+                if (table[i][j] != c) {
+                    colCheck = false;
+                }
+            }
+            if (colCheck) {
+                return c;
+            }
+        }
+        
+//        if (colCheck || rowCheck) {
+//            return c;
+//        }
+    }
+    
+    // check diagonal
+    for (int c = 1; c <= 2; c++) {
+        bool diag = true;
+        bool antidiag = true;
+        
+        for (int i = 0; i < size; i++) {
+            if (table[i][i] != c) {
+                diag = false;
+            }
+            if (table[size-1-i][size-1-i] != c) {
+                antidiag = false;
+            }            
+        }
+        if (diag || antidiag) {
+            return c;
+        }
+    }
+    
+    return -1;
+}
+
+int main(){
+    int rows = 4;
+    int cols = 4;
+    int **table = new int*[rows];
+    for (int i = 0; i < rows; i++) {
+        table[i] = new int[cols];
+    }
+    util::getRandMatrix(table, rows, cols, 0, 2);
+    util::print2DMatrix(table, rows, cols);
+    
+    int success = checkWin(table, rows);
+    std::cout << std::endl;
+    std::cout << success << " is the result" << std::endl;
+    return 0;
+}
