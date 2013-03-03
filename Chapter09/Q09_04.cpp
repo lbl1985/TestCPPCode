b90fdb5dd1a8048b98f95086255d91c55d0f767f
Binlong Li <herbert19lee@gmail.com>
2013-03-02 23:09:09 -0500

    modify the program to make it more concise.

--- Chapter09/Q09_04.cpp
+++ Chapter09/Q09_04.cpp
@@ -21,13 +21,11 @@ std::vector< std::vector<int> > getSubsets(int const *arr, int index){
         subsets.push_back(tmp);
     }else{
         int current = arr[index];
-        subsets = getSubsets(arr, index - 1);
-        std::vector<std::vector<int> > subsetscopy(subsets);
-        std::for_each(subsets.begin(), subsets.end(), [current](std::vector<int> &tmp){
-            tmp.push_back(current);
-        });
-        std::for_each(subsetscopy.cbegin(), subsetscopy.cend(), [&subsets](std::vector<int> const &tmp){
-            subsets.push_back(tmp);
+        std::vector<std::vector<int>> tmp = getSubsets(arr, index - 1);
+        subsets = tmp;
+        std::for_each(tmp.begin(), tmp.end(), [&subsets, current](std::vector<int> &tmp2){
+            tmp2.push_back(current);
+            subsets.push_back(tmp2);
         });
     }
     return subsets;
